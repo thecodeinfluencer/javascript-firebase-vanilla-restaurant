d74b57847d5d26504e4a82611e1c6363f224ab44
thecodeinfluencer <mbenjerminne@gmail.com>
2021-09-20 08:40:23 +0300

    initial

---
+++ js/firebase.js
@@ -0,0 +1,18 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC8FRds1vJNuwlhTgdEP5vMDuXCxsoWI4E",
+    authDomain: "gig-abcrestaurant.firebaseapp.com",
+    projectId: "gig-abcrestaurant",
+    storageBucket: "gig-abcrestaurant.appspot.com",
+    messagingSenderId: "654754379862",
+    appId: "1:654754379862:web:5ff837bbe9870ea267a3f7",
+    measurementId: "G-HWCJGFENK8"
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const analytics = firebaseApp.getAnalytics(app);
